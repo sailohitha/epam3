287ca6f08fd2ec6e9f13cae8b956ae511f5b46f4
Sai Lohitha <Sai Lohitha@DESKTOP-JQTQ84R>
2020-07-22 21:24:41 +0530

    abstract fineshed!!

---
+++ epam2/src/main/java/Epam/epam2/Abstract.java
@@ -0,0 +1,9 @@
+package Epam.epam2;
+
+	public abstract class Abstract {
+		public abstract int total_weight(int a[],int n);
+		public abstract int num_of_candy(int low,int high,int a[],int n);
+
+	}
+
+
---
+++ epam2/src/main/java/Epam/epam2/Apps.java
@@ -0,0 +1,10 @@
+package Epam.epam2;
+
+public class Apps {
+
+	    public static void main( String[] args ) {
+	        System.out.println( "Hello World!" );
+	    }
+	}
+
+
